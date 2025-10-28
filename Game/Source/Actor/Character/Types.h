485dad6c319e547a5983536480f1ee680cfe2546
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-10-28 20:01:52 +0900

    変形エネミーの状態追加。

--- Game/Source/Actor/Character/Types.h
+++ Game/Source/Actor/Character/Types.h
@@ -4,19 +4,28 @@
 
 #pragma once
 enum enPlayerState {
-	enPlayerState_Idle,			// 待機状態。
-	enPlayerState_Walk,			// 歩き状態。
-	enPlayerState_Run,			// 走り状態。
-	enPlayerState_Jump,			// ジャンプ状態。
-	enPlayerState_KnockBack,	// 被ダメージ状態。
-	enPlayerState_KneelDown,	// 死亡状態。
+	enPlayerState_Idle,			// 待機。
+	enPlayerState_Walk,			// 歩き。
+	enPlayerState_Run,			// 走り。
+	enPlayerState_Jump,			// ジャンプ。
+	enPlayerState_KnockBack,	// ノックバック。
+	enPlayerState_KneelDown,	// 死亡。
 	enPlayerState_Num,
 };
 
-enum enEnemyState {
-	enEnemyState_Idle,		// 待機状態。
-	enEnemyState_Walk,		// 歩き状態。
-	enEnemyState_CoolDown,	// クールダウン状態。
-	enEnemyState_Die,		// ダウン状態。
+enum enBasicEnemyState {
+	enEnemyState_Idle,		// 待機。
+	enEnemyState_Chase,		// 追跡。
+	enEnemyState_CoolDown,	// クールダウン。
+	enEnemyState_Die,		// 死亡。
+	enEnemyState_Num,
+};
+
+enum enTransformEnemyState {
+	enEnemyState_Idle,		// 待機。
+	enEnemyState_Escape,	// 逃走。
+	enEnemyState_Transform,	// 変形。
+	enEnemyState_Slide,		// 滑走。
+	enEnemyState_Die,		// 死亡。
 	enEnemyState_Num,
 };
